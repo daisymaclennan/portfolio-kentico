cd38971d02b18883360bd9c74c81a54f31861b33
Daisy Maclennan <daisy.maclennan@distinction.co.uk>
2020-03-17 14:38:35 +0000

    Very basic setup sourcing info from Kentico Kontent

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,18 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: `@kentico/gatsby-source-kontent`,
+        options: {
+          deliveryClientConfig: {
+            projectId: `0458c92c-437f-0002-ed15-5df91931f7eb` // Fill in your Project ID
+          },
+          // Please note that with the Sample Project generated above, `en-US` is the default language for the project and this config. For a blank project, this needs to be `default`.
+          languageCodenames: [
+                  `default`
+          ]
+        }
+      }
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,57 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default ({data}) => {
+  const blogPosts = data.allKontentItemBlogPost.edges
+  const projects = data.allKontentItemProject.edges
+
+  return(
+    <div>
+      <h1>Gatsby site using Kentico</h1>
+      <h2>Blog posts</h2>
+      {blogPosts.map(post => (
+        <h4>{post.node.system.name}</h4>
+      ))}
+      <br />
+
+      <h2>Projects</h2>
+      {projects.map(project => (
+        <h4>{project.node.system.name}</h4>
+      ))}
+    </div>
+  )
+}
+
+export const query = graphql`
+query MyQuery {
+  allKontentItemBlogPost {
+    edges {
+      node {
+        id
+        system {
+          name
+        }
+      }
+    }
+  }
+  allKontentItemProject {
+    edges {
+      node {
+        system {
+          name
+        }
+        elements {
+          featured_image {
+            value {
+              url
+            }
+          }
+          description {
+            value
+          }
+        }
+      }
+    }
+  }
+}
+`
